6b7270b49a4e7eb10824a30fec9a59753dadb2fd
한재원 <hanjo8813@gmail.com>
2022-03-03 21:20:21 +0900

    파일 이사

---
+++ hanjo/개인용/programmers/탐욕법_2_조이스틱/Test.java
@@ -0,0 +1,15 @@
+package 탐욕법_2_조이스틱;
+
+public class Test {
+    public static void main(String[] args){
+
+        String temp = "abc";
+        System.out.println(temp.indexOf("d"));
+
+        
+        System.out.println((int)'A');
+        System.out.println((int)'Z');
+
+    }
+
+}
