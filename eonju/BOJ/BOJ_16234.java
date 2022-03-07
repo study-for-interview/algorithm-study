eff0e75f2dfa15f171bbc227e41e94c220298169
GetMapping <eonju.dev@gmail.com>
2022-03-07 09:07:32 +0900

    unsolve: BOJ_16234 시간초과

--- eonju/BOJ/BOJ_16234.java
+++ eonju/BOJ/BOJ_16234.java
@@ -1,7 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 class BOJ_16234 {
@@ -9,9 +12,9 @@ class BOJ_16234 {
     private static int[] moveI = {-1, 1, 0, 0};
     private static int[] moveJ = {0, 0, -1, 1};
     private static int[][] map;
-    private static int[][] copyMap;
     private static boolean[][] visited;
-    private static boolean flag;
+    private static List<int[]> moveList;
+    private static boolean isMove;
     private static int N;
     private static int L;
     private static int R;
@@ -24,52 +27,50 @@ class BOJ_16234 {
         R = Integer.parseInt(input[2]);
 
         map = new int[N][N];
-        visited = new boolean[N][N];
+        moveList = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
-            input = bufferedReader.readLine().split(" ");
-            for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(input[j]);
-            }
+            map[i] = Arrays.stream(bufferedReader.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
         }
 
-        int year = 0;
+        int day = 0;
         while (true) {
-            flag = false;
+            isMove = false;
+            visited = new boolean[N][N];
+            moveList = new ArrayList<>();
+
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
                     if (!visited[i][j]) {
-                        copyMap = new int[N][N];
-                        visited = new boolean[N][N];
                         bfs(i, j);
                     }
-
                 }
             }
 
-            if (flag) {
-                year++;
+            if (isMove) {
+                day++;
             } else {
                 break;
             }
         }
-
-        System.out.println(year);
-
+        System.out.println(day);
     }
 
     public static void bfs(int startI, int startJ) {
-        Queue<int[]> queue = new LinkedList<>();
         visited[startI][startJ] = true;
+
+        Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[]{startI, startJ});
 
-        int sum = map[startI][startJ];
-        int cnt = 1;
+        int sum = 0;
 
         while (!queue.isEmpty()) {
             int[] poll = queue.poll();
             int nowI = poll[0];
             int nowJ = poll[1];
+            sum += map[nowI][nowJ];
 
             for (int i = 0; i < 4; i++) {
                 int nextI = nowI + moveI[i];
@@ -83,29 +84,26 @@ class BOJ_16234 {
                     continue;
                 }
 
-                if (isInPopulation(map[nowI][nowJ] - map[nextI][nextJ])) {
+                int diff = Math.abs(map[nowI][nowJ] - map[nextI][nextJ]);
+                if (isInPopulation(diff)) {
+                    visited[nextI][nextJ] = true;
                     queue.add(new int[]{nextI, nextJ});
-
-                    copyMap[nextI][nextJ] = map[nextI][nextJ];
-                    sum += map[nextI][nextJ];
-                    cnt++;
-                    flag = true;
+                    moveList.add(new int[]{nextI, nextJ});
+                    isMove = true;
                 }
             }
         }
 
-        movePopulation(sum, cnt);
+        if (isMove) {
+            movePopulation(sum);
+        }
     }
 
-    public static void movePopulation(int sum, int cnt) {
-        int people = sum / cnt;
+    public static void movePopulation(int sum) {
+        int avg = sum / moveList.size();
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (copyMap[i][j] > 0) {
-                    map[i][j] = people;
-                }
-            }
+        for (int[] location : moveList) {
+            map[location[0]][location[1]] = avg;
         }
     }
 
