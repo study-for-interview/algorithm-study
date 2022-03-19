210d2444faca4d214a03e9c455a810a24e77eb25
한재원 <hanjo8813@gmail.com>
2022-03-19 20:17:16 +0900

    ,

---
+++ hanjo/개인용/기타/코테/쏘카_2022_1/문제2/Solution.java
@@ -0,0 +1,22 @@
+package 코테.쏘카_2022_1.문제2;
+
+public class Solution {
+
+    public static int solution(int[] numbers, int k){
+        
+        
+        return 0;
+    }
+
+    public static void dfs(){
+
+    }
+
+    public static void main(String args[]) {
+        // 1
+        System.out.println(solution(new int[]{10, 40, 30, 20}, 20));
+        // 2
+        System.out.println(solution(new int[]{3, 7, 2, 8, 6, 4, 5, 1}, 3));
+    }
+
+}
