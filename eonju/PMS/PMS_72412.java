7e039b60fc884d19f6841fadbe5bd432edad583b
GetMapping <eonju.dev@gmail.com>
2022-03-31 09:31:58 +0900

    solve: BOJ_72412

--- eonju/PMS/PMS_72412.java
+++ eonju/PMS/PMS_72412.java
@@ -1,102 +1,71 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 class Solution {
 
-    public static int[] solution(String[] info, String[] query) {
+    static Map<String, ArrayList<Integer>> allInfo;
+    static ArrayList<Integer> in;
 
-        ArrayList<Person> people = new ArrayList<>();
-        HashMap<String, List<Integer>> langs = new HashMap<>();
-        langs.put("cpp", new ArrayList<>());
-        langs.put("java", new ArrayList<>());
-        langs.put("python", new ArrayList<>());
-        langs.put("-", new ArrayList<>());
-        HashMap<String, List<Integer>> fileds = new HashMap<>();
-        fileds.put("backend", new ArrayList<>());
-        fileds.put("frontend", new ArrayList<>());
-        fileds.put("-", new ArrayList<>());
-        HashMap<String, List<Integer>> levels = new HashMap<>();
-        levels.put("junior", new ArrayList<>());
-        levels.put("senior", new ArrayList<>());
-        levels.put("-", new ArrayList<>());
-        HashMap<String, List<Integer>> foods = new HashMap<>();
-        foods.put("chicken", new ArrayList<>());
-        foods.put("pizza", new ArrayList<>());
-        foods.put("-", new ArrayList<>());
-
-        for (int i = 0; i < info.length; i++) {
-            String[] input = info[i].split(" ");
-            String language = input[0];
-            String filed = input[1];
-            String level = input[2];
-            String food = input[3];
-
-            langs.get(language).add(i);
-            langs.get("-").add(i);
-            fileds.get(filed).add(i);
-            fileds.get("-").add(i);
-            levels.get(level).add(i);
-            levels.get("-").add(i);
-            foods.get(food).add(i);
-            foods.get("-").add(i);
-
-            people.add(new Person(input[0], input[1], input[2], input[3], Integer.parseInt(input[4])));
-        }
+    public int[] solution(String[] info, String[] query) {
 
         int[] answer = new int[query.length];
-        for (int i = 0; i < query.length; i++) {
-            String[] input = query[i].split(" ");
+        allInfo = new HashMap<>();
 
-            String language = input[0];
-            String filed = input[2];
-            String level = input[4];
-            String food = input[6];
-            int score = Integer.parseInt(input[7]);
-
-            List<Integer> tmp = compareList(langs.get(language), fileds.get(filed));
-            tmp = compareList(tmp, levels.get(level));
-            tmp = compareList(tmp, foods.get(food));
-
-            int cnt = 0;
-            for (Integer integer : tmp) {
-                if (people.get(integer).score >= score) {
-                    cnt++;
-                }
-            }
-            answer[i] = cnt;
+        // 1. info 모든 경우의 수 map에 저장
+        for (int i = 0; i < info.length; i++) {
+            dfs("", 0, info[i].split(" "));
         }
 
+        // 2. map에 저장된 점수 list 오름차순으로 정렬
+        List<String> list = new ArrayList<>(allInfo.keySet());
+        for (int i = 0; i < list.size(); i++) {
+            List<Integer> scoreList = allInfo.get(list.get(i));
+            Collections.sort(scoreList);
+        }
+
+        for (int i = 0; i < query.length; i++) {
+            query[i] = query[i].replaceAll(" and ", "");
+            String[] str = query[i].split(" ");
+            int score = Integer.parseInt(str[1]);
+
+            answer[i] = search(str[0], score);
+        }
         return answer;
     }
 
-    public static List<Integer> compareList(List<Integer> arrA, List<Integer> arrB) {
-        ArrayList<Integer> cross = new ArrayList<>();
-        if (arrA.size() == 0 || arrB.size() == 0) {
-            return cross;
-        }
-        for (Integer peopleIdx : arrA) {
-            if (arrB.contains(peopleIdx)) {
-                cross.add(peopleIdx);
+
+    static void dfs(String pos, int depth, String[] info) {
+
+        if (depth == 4) {
+            if (!allInfo.containsKey(pos)) {
+                in = new ArrayList<>();
+                in.add(Integer.parseInt(info[4]));
+                allInfo.put(pos, in);
+            } else {
+                allInfo.get(pos).add(Integer.parseInt(info[4]));
             }
+            return;
         }
-        return cross;
+        dfs(pos + "-", depth + 1, info);
+        dfs(pos + info[depth], depth + 1, info);
+
     }
 
-    static class Person {
-
-        String lang;
-        String field;
-        String level;
-        String food;
-        int score;
-
-        public Person(String lang, String field, String level, String food, int score) {
-            this.lang = lang;
-            this.field = field;
-            this.level = level;
-            this.food = food;
-            this.score = score;
+    // 이분 탐색
+    static int search(String str, int score) {
+        if (!allInfo.containsKey(str)) {
+            return 0;
         }
+        List<Integer> scoreList = allInfo.get(str);
+        int start = 0, end = scoreList.size() - 1;
+        while (start <= end) {
+
+            int mid = (start + end) / 2;
+            if (scoreList.get(mid) < score) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return scoreList.size() - start;
     }
 }
