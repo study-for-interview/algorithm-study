00813832430768f6aa9163e0c6d4925f36e71906
GetMapping <eonju.dev@gmail.com>
2022-04-04 13:46:57 +0900

    unsolve: BOJ_20058

--- eonju/BOJ/BOJ_20058.java
+++ eonju/BOJ/BOJ_20058.java
@@ -1,173 +1,129 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
-class BOJ_20058 {
+public class BOJ_20058 {
 
-    private static int[] moveHeight = {-1, 1, 0, 0};
-    private static int[] moveWidth = {0, 0, -1, 1};
-    public static int mapSize = 0;
-    private static int[] targetSize;
-    public static int[][] map;
-    public static boolean[][] visited;
-    private static int maxCnt;
-    private static int maxIce;
+    static int N, Q, land, totalIce;
+    static int[][] A;
+    static int[] L;
+    static int[][] deltas = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = bufferedReader.readLine().split(" ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        mapSize = (int) Math.pow(2, Integer.parseInt(input[0]));
-        int levelCnt = Integer.parseInt(input[1]);
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        Q = Integer.parseInt(st.nextToken());
+        N = (int) Math.pow(2, N);
 
-        map = new int[mapSize][mapSize];
-        for (int i = 0; i < mapSize; i++) {
-            input = bufferedReader.readLine().split(" ");
-            for (int j = 0; j < mapSize; j++) {
-                map[i][j] = Integer.parseInt(input[j]);
+        A = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        targetSize = new int[levelCnt];
-        input = bufferedReader.readLine().split(" ");
-        for (int i = 0; i < levelCnt; i++) {
-            targetSize[i] = (int) Math.pow(2, Integer.parseInt(input[i]));
+        L = new int[Q];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < Q; i++) {
+            L[i] = Integer.parseInt(st.nextToken());
         }
-
-        for (int i = 0; i < targetSize.length; i++) {
-            turnMap(targetSize[i]);
-            melt();
+        for (int i = 0; i < Q; i++) {
+            A = divide(L[i]);
+            A = melt();
         }
-
-        maxCnt = 0;
-        maxIce = 0;
-        visited = new boolean[mapSize][mapSize];
-
-        for (int i = 0; i < mapSize; i++) {
-            for (int j = 0; j < mapSize; j++) {
-                if (map[i][j] > 0 && !visited[i][j]) {
-                    bfs(i, j);
-                }
-            }
-        }
-
-        System.out.println(maxIce);
-        System.out.println(maxCnt);
+        findBiggest();
+        System.out.println(totalIce);
+        System.out.println(land);
     }
 
-    public static void turnMap(int targetSize) {
-        int[][] tmpMap = new int[mapSize][mapSize];
+    static int[][] divide(int L) {
+        int[][] temp = new int[N][N];
+        L = (int) Math.pow(2, L);
 
-        for (int startI = 0; startI < mapSize; startI += targetSize) {
-            for (int startJ = 0; startJ < mapSize; startJ += targetSize) {
-                for (int i = startI; i < startI + targetSize; i++) {
-                    int maxHeightIdx = startI + targetSize - 1;
-                    for (int j = startJ; j < startJ + targetSize; j++) {
-                        int maxWidthIdx = startJ + targetSize - 1;
-                        if ((maxHeightIdx - i) + (maxWidthIdx - j) == targetSize - 1) {
-                            int diff;
-                            if (i < (maxHeightIdx + 1) / 2) {
-                                diff = (maxWidthIdx - startJ) - (i*2);
-                            } else {
-                                diff = (maxWidthIdx - startJ) + (i*2);
-                            }
-                            if (j + diff < startJ + targetSize && isInBound(i, j + diff)) {
-                                tmpMap[i][j + diff] = map[i][j];
-                                continue;
-                            }
-
-                            if (i + diff < startJ + targetSize && isInBound(i + diff, j)) {
-                                tmpMap[i + diff][j] = map[i][j];
-                                continue;
-                            }
-
-                            if (j - diff >= startJ && isInBound(i, j - diff)) {
-                                tmpMap[i][j - diff] = map[i][j];
-                                continue;
-                            }
-
-                            if (i - diff >= startI && isInBound(i - diff, j)) {
-                                tmpMap[i - diff][j] = map[i][j];
-                            }
-
-                        } else {
-                            tmpMap[j][i] = map[i][j];
-                        }
-                    }
-                }
+        for (int i = 0; i < N; i += L) {
+            for (int j = 0; j < N; j += L) {
+                rotate(i, j, L, temp);
             }
+        }
+        return temp;
+    }
 
-            map = tmpMap;
+    static void rotate(int r, int c, int L, int[][] temp) {
+        for (int i = 0; i < L; i++) {
+            for (int j = 0; j < L; j++) {
+                temp[r + j][c + L - i - 1] = A[r + i][c + j];
+            }
         }
     }
 
-    public static void melt() {
-        for (int i = 0; i < mapSize; i++) {
-            for (int j = 0; j < mapSize; j++) {
+    static int[][] melt() {
+        int[][] temp = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            temp[i] = Arrays.copyOf(A[i], N);
+        }
+
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
                 int cnt = 0;
-
-                for (int k = 0; k < 4; k++) {
-                    int nextI = i + moveHeight[k];
-                    int nextJ = j + moveWidth[k];
-
-                    if (!isInBound(nextI, nextJ)) {
-                        continue;
-                    }
-
-                    if (map[nextI][nextJ] > 0) {
+                if (A[r][c] == 0) {
+                    continue;
+                }
+                for (int i = 0; i < 4; i++) {
+                    int nr = r + deltas[i][0];
+                    int nc = c + deltas[i][1];
+                    if (isIn(nr, nc) && A[nr][nc] > 0) {
                         cnt++;
                     }
                 }
-
                 if (cnt < 3) {
-                    map[i][j] = map[i][j] - 1;
+                    temp[r][c]--;
+                }
+            }
+        }
+        return temp;
+    }
+
+    static void findBiggest() {
+        boolean[][] visited = new boolean[N][N];
+        Stack<int[]> stack = new Stack<>();
+        land = 0;
+        totalIce = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (A[i][j] > 0 && !visited[i][j]) {
+                    stack.push(new int[]{i, j});
+                    visited[i][j] = true;
+                    totalIce += A[i][j];
+                    int count = 1;
+                    while (!stack.isEmpty()) {
+                        int[] temp = stack.pop();
+                        int r = temp[0];
+                        int c = temp[1];
+
+                        for (int k = 0; k < 4; k++) {
+                            int nr = r + deltas[k][0];
+                            int nc = c + deltas[k][1];
+
+                            if (isIn(nr, nc) && A[nr][nc] > 0 && !visited[nr][nc]) {
+                                count++;
+                                visited[nr][nc] = true;
+                                stack.push(new int[]{nr, nc});
+                                totalIce += A[nr][nc];
+                            }
+                        }
+                    }
+                    land = Math.max(count, land);
+
                 }
             }
         }
     }
 
-    public static void bfs(int startI, int startJ) {
-        Queue<int[]> queue = new LinkedList<>();
-        int cnt = 0;
-        int sum = 0;
-
-        queue.add(new int[]{startI, startJ});
-        visited[startI][startJ] = true;
-
-        while (!queue.isEmpty()) {
-            int[] poll = queue.poll();
-            cnt++;
-            sum += map[poll[0]][poll[1]];
-            int nowHeight = poll[0];
-            int nowWidth = poll[1];
-
-            for (int i = 0; i < 4; i++) {
-                int nextI = nowHeight + moveHeight[i];
-                int nextJ = nowWidth + moveWidth[i];
-
-                if (!isInBound(nextI, nextJ)) {
-                    continue;
-                }
-
-                if (visited[nextI][nextJ]) {
-                    continue;
-                }
-
-                if (map[nextI][nextJ] > 0) {
-                    queue.add(new int[]{nextI, nextJ});
-                    visited[nextI][nextJ] = true;
-                }
-            }
-        }
-
-        maxCnt = Math.max(cnt, maxCnt);
-        maxIce = maxIce + sum;
+    static boolean isIn(int r, int c) {
+        return r >= 0 && r < N && c >= 0 && c < N;
     }
-
-    public static boolean isInBound(int i, int j) {
-        return i >= 0 && j >= 0 && i < mapSize && j < mapSize;
-    }
-
 }
