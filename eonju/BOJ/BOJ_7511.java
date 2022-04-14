f55a88aaf5d31af778e9999c2bffc09bb16002e5
GetMapping <eonju.dev@gmail.com>
2022-04-14 17:31:26 +0900

    solve: BOJ_7511

--- eonju/BOJ/BOJ_7511.java
+++ eonju/BOJ/BOJ_7511.java
@@ -1,50 +1,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int testCaseQuantity = Integer.parseInt(bufferedReader.readLine());
+
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int testCaseQuantity = Integer.parseInt(stringTokenizer.nextToken());
 
         for (int testCase = 1; testCase <= testCaseQuantity; testCase++) {
-            int userQuantity = Integer.parseInt(bufferedReader.readLine());
-            int relationQuantity = Integer.parseInt(bufferedReader.readLine());
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int userQuantity = Integer.parseInt(stringTokenizer.nextToken());
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int relationQuantity = Integer.parseInt(stringTokenizer.nextToken());
 
-            int[] parent = new int[userQuantity];
+            int[] parent = new int[userQuantity + 1];
             for (int number = 0; number < parent.length; number++) {
                 parent[number] = number;
             }
 
             for (int relation = 0; relation < relationQuantity; relation++) {
-                String[] input = bufferedReader.readLine().split(" ");
-
-                int userA = Integer.parseInt(input[0]);
-                int userB = Integer.parseInt(input[1]);
+                stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+                int userA = Integer.parseInt(stringTokenizer.nextToken());
+                int userB = Integer.parseInt(stringTokenizer.nextToken());
 
                 union(userA, userB, parent);
             }
 
-            System.out.println("Scenario " + testCase + ":");
+            StringBuilder sb = new StringBuilder();
+            sb.append("Scenario ");
+            sb.append(testCase);
+            sb.append(":\n");
 
-            int solveQuantity = Integer.parseInt(bufferedReader.readLine());
+
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int solveQuantity = Integer.parseInt(stringTokenizer.nextToken());
 
             for (int solve = 0; solve < solveQuantity; solve++) {
-                String[] input = bufferedReader.readLine().split(" ");
-                int userA = Integer.parseInt(input[0]);
-                int userB = Integer.parseInt(input[1]);
+                stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+                int userA = Integer.parseInt(stringTokenizer.nextToken());
+                int userB = Integer.parseInt(stringTokenizer.nextToken());
 
                 int rootA = find(userA, parent);
                 int rootB = find(userB, parent);
 
                 if (rootA == rootB) {
-                    System.out.println(1);
+                    sb.append("1\n");
                 } else {
-                    System.out.println(0);
+                    sb.append("0\n");
                 }
             }
-            System.out.println();
+
+            System.out.println(sb);
         }
     }
 
