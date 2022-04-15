d6022f00f879941d924ba91ba6788d186b914363
GetMapping <eonju.dev@gmail.com>
2022-04-15 18:46:17 +0900

    solve: BOJ_92342

--- eonju/PMS/PMS_92342.java
+++ eonju/PMS/PMS_92342.java
@@ -1,74 +1,70 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 class Solution {
 
-    private static int[] ryan;
-    private static int[] answer;
-    private static int[] apeach;
-    private static int N;
-    private static int maxDiff;
+    static ArrayList<int[]> answer = new ArrayList<>();
+    static int[] ryan;
+    static int[] apeach;
+    static int N;
+    static int max = Integer.MIN_VALUE;
 
-    public static void main(String[] args) {
-        int[] answer1 = solution(5, new int[]{2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0});
-        int[] answer2 = solution(1, new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
-        int[] answer3 = solution(9, new int[]{0, 0, 1, 2, 0, 1, 1, 1, 1, 1, 1});
-        int[] answer4 = solution(10, new int[]{0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 3});
+    public static void DFS(int L, int s) {
+        if (L == N) {
+            int apeachScore = 0;
+            int ryanScore = 0;
 
-        System.out.println(Arrays.toString(Arrays.stream(answer1).toArray()));
-        System.out.println(Arrays.toString(Arrays.stream(answer2).toArray()));
-        System.out.println(Arrays.toString(Arrays.stream(answer3).toArray()));
-        System.out.println(Arrays.toString(Arrays.stream(answer4).toArray()));
-    }
-
-
-    public static int[] solution(int n, int[] info) {
-        N = n;
-        ryan = new int[11];
-        answer = new int[11];
-        apeach = info.clone();
-        maxDiff = 0;
-
-        dfs(0, 0);
-
-        if (maxDiff == 0) {
-            answer = new int[]{-1};
-        }
-
-        return answer;
-    }
-
-    public static void dfs(int depth, int start) {
-        if (depth == N) {
-            calcScore(apeach, ryan);
-            return;
-        }
-
-        for (int i = start; i < 11; i++) {
-            if (N - (apeach[i] + 1) >= 0) {
-                ryan[i] = apeach[i] + 1;
-                dfs(depth + ryan[i], start + 1);
-                ryan[i] = 0;
-            }
-        }
-    }
-
-    public static void calcScore(int[] apeach, int[] ryan) {
-        int apeachScore = 0;
-        int ryanScore = 0;
-
-        for (int i = 0; i < 11; i++) {
-            if (apeach[i] != 0 && ryan[i] != 0) {
+            for (int i = 0; i <= 10; i++) {
+                if (apeach[i] == 0 && ryan[i] == 0) {
+                    continue;
+                }
                 if (apeach[i] < ryan[i]) {
                     ryanScore += (10 - i);
                 } else {
                     apeachScore += (10 - i);
                 }
             }
+
+            if (ryanScore > apeachScore) {
+                int diff = ryanScore - apeachScore;
+
+                if (diff > max) {
+                    max = diff;
+                    answer.clear();
+                    answer.add(ryan.clone());
+                } else if (diff == max) {
+                    answer.add(ryan.clone());
+                }
+            }
+        } else {
+            for (int i = s; i < 11; i++) {
+                ryan[i]++;
+                DFS(L + 1, i);
+                ryan[i]--;
+            }
+        }
+    }
+
+    public static int[] solution(int n, int[] info) {
+        N = n;
+        apeach = info.clone();
+        ryan = new int[11];
+
+        DFS(0, 0);
+
+        if (answer.isEmpty()) {
+            return new int[]{-1};
         }
 
-        if (apeachScore < ryanScore && ryanScore >= maxDiff) {
-            maxDiff = ryanScore;
-            answer = ryan.clone();
-        }
+        Collections.sort(answer, (o1, o2) -> {
+            for (int i = 10; i >= 0; i--) {
+                if (o1[i] != o2[i]) {
+                    return o2[i] - o1[i];
+                }
+            }
+            return 0;
+        });
+
+        return answer.get(0);
     }
 }
