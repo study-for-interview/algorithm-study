45494e1f5ebbe142694523e1a516c0afecb364ff
GetMapping <eonju.dev@gmail.com>
2022-04-19 09:06:55 +0900

    solve: BOJ_7490

--- eonju/BOJ/BOJ_7490.java
+++ eonju/BOJ/BOJ_7490.java
@@ -1,91 +1,64 @@
+import static java.util.stream.Collectors.toList;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Arrays;
+import java.util.Iterator;
 
 class Main {
 
-    private static ArrayList<Integer> numbers;
-    private static ArrayList<String> answers;
-    private static boolean[] visited;
-    private static int N;
+    static int N;
+    static StringBuilder answer = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int testCaseQuantity = Integer.parseInt(bufferedReader.readLine());
 
-        for (int i = 0; i < testCaseQuantity; i++) {
+        int t = Integer.parseInt(bufferedReader.readLine());
+
+        for (int i = 0; i < t; i++) {
             N = Integer.parseInt(bufferedReader.readLine());
-            numbers = new ArrayList<>();
-            answers = new ArrayList<>();
-            visited = new boolean[N + 1];
-            for (int number = 1; number <= N; number++) {
-                numbers.add(number);
-            }
-
-            visited[1] = true;
-            backtracking(2, new StringBuilder().append(1));
-
-            for (int idx = 0; idx < answers.size(); idx++) {
-                System.out.println(answers.get(idx));
-            }
-            System.out.println();
+            comb(1, 1, "1");
+            answer.append("\n");
         }
+
+        System.out.println(answer.toString());
     }
 
-    public static void backtracking(int depth, StringBuilder sb) {
-        if (depth == N + 1) {
-            if (calculate(sb) == 0) {
-                answers.add(String.valueOf(sb));
+    static void comb(int startNumber, int depth, String sik) {
+        if (depth == N) {
+            if (calculate(sik) == 0) {
+                answer.append(sik + "\n");
             }
             return;
         }
 
-        for (int i = depth; i <= N; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                backtracking(depth + 1, new StringBuilder(sb).append("+").append(i));
-                visited[i] = false;
-                visited[i] = true;
-                backtracking(depth + 1, new StringBuilder(sb).append(" ").append(i));
-                visited[i] = false;
-                visited[i] = true;
-                backtracking(depth + 1, new StringBuilder(sb).append("-").append(i));
-                visited[i] = false;
-            }
-        }
+        comb(startNumber + 1, depth + 1, sik + ' ' + (startNumber + 1));
+        comb(startNumber + 1, depth + 1, sik + '+' + (startNumber + 1));
+        comb(startNumber + 1, depth + 1, sik + '-' + (startNumber + 1));
+
     }
 
-    public static int calculate(StringBuilder sb) {
-        LinkedList<String> queue = new LinkedList<>();
 
-        String[] split = sb.toString().split("");
-        for (int i = 0; i < split.length; i++) {
-            String target = split[i];
+    public static int calculate(String sik) {
+        sik = sik.replaceAll(" ", "");
 
-            if (target.equals(" ")) {
-                queue.add(String.valueOf(Integer.parseInt(queue.removeLast()) * Integer.parseInt(split[i + 1])));
-                i++;
-                continue;
-            }
+        Iterator<Integer> iterator = Arrays.stream(sik.split("[+,-]"))
+            .map(Integer::parseInt)
+            .collect(toList())
+            .iterator();
 
-            queue.add(target);
-        }
+        int result = iterator.next();
 
-        int sum = Integer.parseInt(queue.poll());
-
-        while (!queue.isEmpty()) {
-            String poll = queue.poll();
-
-            if (poll.equals("+")) {
-                sum += Integer.parseInt(queue.poll());
-            } else if (poll.equals("-")) {
-                sum -= Integer.parseInt(queue.poll());
+        for (int i = 0; i < sik.length(); i++) {
+            if (sik.charAt(i) == '+') {
+                result += iterator.next();
+            } else if (sik.charAt(i) == '-') {
+                result -= iterator.next();
             }
         }
 
-        return sum;
+        return result;
     }
 
 }
