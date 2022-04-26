4f3f03cc0673a29648a524a1759cbb05a99a28a7
한재원 <hanjo8813@gmail.com>
2022-04-26 09:53:39 +0900

    정리

---
+++ hanjo/개인용/기타/test/Test8.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Test8 {
+    public static void main(String[] args) {
+
+        char c = 0;
+        System.out.println(c);
+        
+        c = 0 + '0';
+        System.out.println(c);
+    }
+}
