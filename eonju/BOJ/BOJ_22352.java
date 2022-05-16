e22eea225ad6d68a0eb6a6876915ff27e3bf9206
GetMapping <eonju.dev@gmail.com>
2022-05-16 21:12:08 +0900

    solve: BOJ_22352

--- eonju/BOJ/BOJ_22352.java
+++ eonju/BOJ/BOJ_22352.java
@@ -1,92 +1,110 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
-class Main {
+public class Main {
+
+    static int N, M;
+    static int[][] beforeMap;
+    static int[][] afterMap;
+
+    static int[] moveX = {1, -1, 0, 0};
+    static int[] moveY = {0, 0, 1, -1};
+
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = bufferedReader.readLine().split(" ");
-        int height = Integer.parseInt(input[0]);
-        int width = Integer.parseInt(input[1]);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int[][] beforeMap = new int[height][width];
-        int[][] afterMap = new int[height][width];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < height; i++) {
-            beforeMap[i] = Arrays.stream(bufferedReader.readLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+        beforeMap = new int[N][M];
+        afterMap = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                beforeMap[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
-        for (int i = 0; i < height; i++) {
-            afterMap[i] = Arrays.stream(bufferedReader.readLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                afterMap[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
-
-        int[][] diffMap = diff(beforeMap, afterMap);
-        boolean answer = bfs(diffMap);
-
-        if (answer) {
+        start();
+        if (check()) {
             System.out.println("YES");
         } else {
             System.out.println("NO");
         }
+
     }
 
-    public static boolean bfs(int[][] diffMap) {
-        int cnt = 0;
-
-        for (int i = 0; i < diffMap.length; i++) {
-            for (int j = 0; j < diffMap[i].length; j++) {
-                if (diffMap[i][j] != 0) {
-                    cnt++;
-                    change(i, j, diffMap[i][j], diffMap);
+    public static void start() {
+        for (int y = 0; y < N; y++) {
+            for (int x = 0; x < M; x++) {
+                if (beforeMap[y][x] != afterMap[y][x]) {
+                    BFS(x, y, afterMap[y][x]);
+                    return;
                 }
             }
         }
-
-        return cnt < 2;
     }
 
-    public static void change(int startI, int startJ, int data, int[][] diffMap) {
-        int[][] move = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-        Queue<int[]> queue = new LinkedList<>(); // i, j
+    public static boolean check() {
+        for (int y = 0; y < N; y++) {
+            for (int x = 0; x < M; x++) {
+                if (beforeMap[y][x] != afterMap[y][x]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
-        queue.add(new int[]{startI, startJ});
-        diffMap[startI][startJ] = 0;
+    public static void BFS(int x, int y, int val) {
+        Queue<Point> q = new LinkedList<>();
+        q.add(new Point(x, y));
 
-        while (!queue.isEmpty()) {
-            int[] poll = queue.poll();
-            for (int i = 0; i < move.length; i++) {
-                int nextI = poll[0] + move[i][0];
-                int nextJ = poll[1] + move[i][1];
+        boolean[][] visited = new boolean[N][M];
+        visited[y][x] = true;
 
-                if (nextI < 0 || nextJ < 0 || nextI >= diffMap.length || nextJ >= diffMap[0].length) {
+        int lastVal = beforeMap[y][x];
+        beforeMap[y][x] = val;
+
+        while (!q.isEmpty()) {
+            Point cur = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nextX = cur.x + moveX[i];
+                int nextY = cur.y + moveY[i];
+
+                if (nextX < 0 || nextX >= M || nextY < 0 || nextY >= N) {
                     continue;
                 }
-
-                if (diffMap[nextI][nextJ] == data) {
-                    queue.add(new int[]{nextI, nextJ});
-                    diffMap[nextI][nextJ] = 0;
+                if (!visited[nextY][nextX] && beforeMap[nextY][nextX] == lastVal) {
+                    q.add(new Point(nextX, nextY));
+                    visited[nextY][nextX] = true;
+                    beforeMap[nextY][nextX] = val;
                 }
             }
         }
     }
+}
 
-    public static int[][] diff(int[][] beforeMap, int[][] afterMap) {
-        int[][] diffMap = new int[beforeMap.length][beforeMap[0].length];
-        for (int i = 0; i < beforeMap.length; i++) {
-            for (int j = 0; j < beforeMap[i].length; j++) {
-                if (beforeMap[i][j] != afterMap[i][j]) {
-                    diffMap[i][j] = afterMap[i][j];
-                }
-            }
-        }
+class Point {
 
-        return diffMap;
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
