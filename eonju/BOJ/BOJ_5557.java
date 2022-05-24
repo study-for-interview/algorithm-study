208b99c66f2158f1dde77dd3db994b570612fef4
GetMapping <eonju.dev@gmail.com>
2022-05-24 09:50:20 +0900

    solve: BOJ_5557

--- eonju/BOJ/BOJ_5557.java
+++ eonju/BOJ/BOJ_5557.java
@@ -4,39 +4,45 @@ import java.io.InputStreamReader;
 
 class Main {
 
-    private static int answer;
+    private static int[] numbers;
+    private static long[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int numberCnt = Integer.parseInt(bufferedReader.readLine());
 
-        int[] numbers = new int[numberCnt];
+        numbers = new int[numberCnt];
 
         String[] input = bufferedReader.readLine().split(" ");
         for (int i = 0; i < input.length; i++) {
             numbers[i] = Integer.parseInt(input[i]);
         }
 
-        answer = 0;
-        count(1, numbers, numbers[0], numbers[numbers.length - 1]);
-        System.out.println(answer);
+        dp = new long[21];
+        dp[numbers[0]] = 1;
+        solve(1);
+        System.out.println(dp[numbers[numbers.length - 1]]);
     }
 
-    public static void count(int idx, int[] numbers, int sum, int target) {
+    public static void solve(int idx) {
         if (idx == numbers.length - 1) {
-            if (sum == target) {
-                answer++;
-            }
-
             return;
         }
 
-        if (sum + numbers[idx] >= 0 && sum + numbers[idx] <= 20) {
-            count(idx + 1, numbers, sum + numbers[idx], target);
+        long temp[] = new long[21];
+        for (int i = 0; i < dp.length; i++) {
+            if (dp[i] != 0) {
+                if (i + numbers[idx] <= 20) {
+                    temp[i + numbers[idx]] += dp[i];
+                }
+                if (i - numbers[idx] >= 0) {
+                    temp[i - numbers[idx]] += dp[i];
+                }
+            }
         }
 
-        if (sum - numbers[idx] >= 0 && sum - numbers[idx] <= 20) {
-            count(idx + 1, numbers, sum - numbers[idx], target);
-        }
+        dp = temp.clone();
+        solve(idx + 1);
     }
+
 }
